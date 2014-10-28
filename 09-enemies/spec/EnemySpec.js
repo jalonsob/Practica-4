6109fec7fabd0bf68021b183123344933dfd459c
jalonsob <jalonsob@gmail.com>
2014-10-28 19:55:26 +0100

    Acabadas pruebas del Enemy.js y añadido datos de la practica anterior

--- 09-enemies/spec/EnemySpec.js
+++ 09-enemies/spec/EnemySpec.js
@@ -1,63 +1,65 @@
-/*
+describe("Clase EnemySpec", function(){
+  var canvas, ctx;
 
-  Requisitos:
+  beforeEach(function(){
+    loadFixtures('index.html');
 
-  El objetivo de este prototipo es añadir al juego naves enemigas. Las
-  naves se añadirán al tablero de juegos (objeto GameBoard) al igual
-  que el resto de los elementos del juego (nave del jugador y
-  misiles).
+    canvas = $('#game')[0];
+    expect(canvas).toExist();
 
-  Cada nave enemiga debe tener un patrón de movimiento que exhibirá
-  desde que entra por la parte superior del canvas hasta que
-  desaparece por la parte inferior. En este prototipo las naves
-  enemigos no interaccionan con el resto de los elementos del juego:
-  los disparos de la nave del jugador no les afectan. La nave del
-  jugador tampoco se ve afectada por la colisión con una nave enemiga.
+    ctx = canvas.getContext('2d');
+    expect(ctx).toBeDefined();
+
+    //Creo los sprites para todo tipo de objetos en el juego, si no los creo puedo tener quejas en this.sprite, linea 235 y 234 del game.js
+    SpriteSheet = {
+          map : {missile: { sx: 0, sy: 30, w: 2, h: 10, frames: 1 },
+                  ship: { sx: 0, sy: 0, w: 37, h: 42, frames: 1 },
+                  enemy_purple: { sx: 37, sy: 0, w: 42, h: 43, frames: 1 },
+                  enemy_bee: { sx: 79, sy: 0, w: 37, h: 43, frames: 1 },
+                  enemy_ship: { sx: 116, sy: 0, w: 42, h: 43, frames: 1 },
+                  enemy_circle: { sx: 158, sy: 0, w: 32, h: 33, frames: 1 }
+          draw: function(){},
+        };
+  
+    });
+
+  it("draw",function(){
+
+        //Es mejor crearme el tipo basico de enemigo aqui, porque si uso el de game.js puede que obtenga errores no deseados al usar basura
+        //Si hago las pruebas del chasis de un coche, hago la prueba del chasis y no le meto el motor dentro
+        basic= { x: 100, y: -50, sprite: 'enemy_purple', B: 100, C: 2 , E: 100 }
+
+        var gameb = new GameBoard();
+        enemy= new Enemy(basic);
+        
+        gameb.add(gameb);
+        gameb.add(new Enemy(basic, { x: 200 })); 
+
+        spyOn(SpriteSheet, "draw");
+
+        enemy.draw(ctx);
+        expect(SpriteSheet.draw).toHaveBeenCalled();
+        expect(SpriteSheet.draw.calls[0].args[1]).toEqual("enemy_purple");
+        expect(SpriteSheet.draw.calls[0].args[2]).toEqual(enemy.x);
+        expect(SpriteSheet.draw.calls[0].args[3]).toEqual(enemy.y);   
+    });
+
+    it("step",function(){
+
+        var gameb = new GameBoard();
+        
+        enemy= new Enemy(basic);
+        
+        gameb.add(gameb);
+        gameb.add(new Enemy(basic, { x: 200 })); 
+
+        spyOn(SpriteSheet, "draw");
+    
+        gameb.step(1);
+        
+        expect(gameb.draw).toHaveBeenCalled();
+ 
+    });
 
 
-  Especificación:
-
-  1. El patrón de movimiento lo dictan las ecuaciones que se
-     utilizarán para calcular las componentes vx e vy de su velocidad.
-     Los parámetros de las ecuaciones que definen vx e vy determinan
-     el patrón de comportamiento:
-
-     vx = A + B * sin (C * t + D) 
-     vy = E + F * sin (G * t + H)
-
-     siendo t la edad de un enemigo, calculada como el tiempo que ha
-     pasado desde que se creó la nave.
-
-     A: componente constante de la velocidad horizontal
-     B: fuerza de la velocidad horizontal sinusoidal
-     C: periodo de la velocidad horizontal sinusoidal
-     D: desplazamiento en el tiempo de la velocidad horizontal
-        sinusoidal
-
-     E: componente constante de la velocidad vertical
-     F: fuerza de la velocidad vertical sinusoidal
-     G: periodo de la velocidad vertical sinusoidal
-     H: desplazamiento en el tiempo de la velocidad vertical
-        sinusoidal
-
-     Todos estos parámetros tendrán un valor por defecto de 0
-     (definido en la variable baseParameters en el constructor), que
-     puede ser substituido por otro valor cuando se crea la nave.
-
-
-  2. Se creará un nuevo constructor/clase Enemy. Los enemigos se
-     diferenciarán sólo en su posición inicial, en el sprite que
-     utilizan y en el patrón de movimiento (parámetros A..H de la
-     velocidad), pero todos serán de la misma clase: Enemy.
-
-     Para definir diferentes tipos de enemigos se pasará al
-     constructor una plantilla con valores para las propiedades (x, y,
-     sprite, A..H).
-
-     Para poder definir fácilmente enemigos parecidos creados a partir
-     de una misma plantilla, se pasará un segundo argumento al
-     constructor con valores alternativos para algunas de las
-     propiedades de la plantilla.
-
-*/
-
+});
