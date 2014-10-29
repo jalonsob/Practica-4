8530ec2f3dd9ab8082192b0a922eee8e789e67c8
jalonsob <jalonsob1991@gmail.com>
2014-10-29 12:03:57 +0100

    Añadidas las pruebas del ejercicio anterior, todas funcionan

--- 10-refactor/spec/GameBoardSpec.js
+++ 10-refactor/spec/GameBoardSpec.js
@@ -1,58 +1,124 @@
-/*
+describe("Clase GameBoardSpec", function(){
+	var canvas, ctx;
 
-  En el anterior prototipo (06-player), el objeto Game permite
-  gestionar una colección de tableros (boards). Los tres campos de
-  estrellas, la pantalla de inicio, y el sprite de la nave del
-  jugador, se añaden como tableros independientes para que Game pueda
-  ejecutar sus métodos step() y draw() periódicamente desde su método
-  loop(). Sin embargo los objetos que muestran los tableros no pueden
-  interaccionar entre sí. Aunque se añadiesen nuevos tableros para los
-  misiles y para los enemigos, resulta difícil con esta arquitectura
-  pensar en cómo podría por ejemplo detectarse la colisión de una nave
-  enemiga con la nave del jugador, o cómo podría detectarse si un
-  misil disparado por la nave del usuario ha colisionado con una nave
-  enemiga.
+	beforeEach(function(){
+		loadFixtures('index.html');
+
+		canvas = $('#game')[0];
+		expect(canvas).toExist();
+
+		ctx = canvas.getContext('2d');
+		expect(ctx).toBeDefined();
+	
+   	});
+
+   	
+   	it("add",function(){
+   		var obj="foo";
+   		var gameb=  new GameBoard();
+   		gameb.add(obj);
+   		expect(gameb.objects).toEqual(['foo']);  	
+   	});
+   	
+   	it("resetRemoved",function(){
+   		
+   		var gameb=  new GameBoard();
+   		
+   		gameb.resetRemoved();
+   		
+   		expect(gameb.removed).toBeDefined();  	
+   	});
+   	
+   	it("removed",function(){
+   		var obj="foo";
+   		var gameb=  new GameBoard();
+   		
+   		gameb.add(obj);
+   		gameb.resetRemoved();
+   		gameb.remove(obj);
+   		
+   		expect(gameb.removed[0]).toEqual('foo');  	
+   	});
+   	
+   	it("finalizeRemoved",function(){
+   		var obj="foo";
+   		var gameb=  new GameBoard();
+   		
+   		gameb.add(obj);
+   		gameb.resetRemoved();
+   		gameb.remove(obj);
+   		gameb.finalizeRemoved();
+   		
+   		expect(gameb.objects[0]).toEqual(undefined);  	
+   	});
+   	
+   	it("iterate",function(){
+   		
+   		var gameb = new GameBoard();
+		spyOn(gameb, "iterate"); 
+
+		gameb.iterate("resetRemoved", ctx);
+
+		expect(gameb.iterate).toHaveBeenCalled();
+   	});
+   	
+   	it("detect",function(){
+   		
+   		var obj="foo";
+   		var gameb = new GameBoard();
+   		
+   		var testdet= function(objx){
+   			objx=="foo";
+   		}
+	
+		gameb.add(obj);
+		spyOn(gameb, "detect");
+		
+		gameb.detect(testdet);
+		
+		expect(gameb.detect).toHaveBeenCalledWith(testdet);
+		expect(gameb.detect).toBeTruthy();
+		
+   	});
+   	
+   	it ("step", function(){
+		var gameb = new GameBoard();
+		var dt = 30 / 1000;
+		
+		spyOn(gameb, "iterate");
+		gameb.step(dt);
+	
+		expect(gameb.iterate).toHaveBeenCalledWith('step',dt);		
+	});
+	
+	it ("draw", function(){
+		var gameb = new GameBoard();
+		var ct = "foo";
+		
+		spyOn(gameb, "iterate");
+		gameb.draw(ct);
+	
+		expect(gameb.iterate).toHaveBeenCalledWith('draw',ct);		
+	});
+	
+	it ("overlap", function(){
+		var gameb = new GameBoard();
+		var obj1 = {w:3,h:3,x:0,y:0};
+		var obj2 = {w:3,h:3,x:1,y:1};
+	
+		expect(gameb.overlap(obj1,obj2)).toBeTruthy();
+	});
+	
+	it ("collide", function(){
+		var gameb = new GameBoard();
+		var obj1 = {w:3,h:3,x:0,y:0};
+	
+		expect(gameb.collide(obj1)).toBeFalsy();
+	});
 
 
-  Requisitos:
-
-  Este es precisamente el requisito que se ha identificado para este
-  prototipo: diseñar e implementar un mecanismo que permita gestionar
-  la interacción entre los elementos del juego. Para ello se diseñará
-  la clase GameBoard. Piensa en esta clase como un tablero de un juego
-  de mesa, sobre el que se disponen los elementos del juego (fichas,
-  cartas, etc.). En Alien Invasion los elementos del juego serán las
-  naves enemigas, la nave del jugador y los misiles. Para el objeto
-  Game, GameBoard será un board más, por lo que deberá ofrecer los
-  métodos step() y draw(), siendo responsable de mostrar todos los
-  objetos que contenga cuando Game llame a estos métodos.
-
-  Este prototipo no añade funcionalidad nueva a la que ofrecía el
-  prototipo 06.
+});
 
 
-  Especificación: GameBoard debe
 
-  - mantener una colección a la que se pueden añadir y de la que se
-    pueden eliminar sprites como nave enemiga, misil, nave del
-    jugador, explosión, etc.
 
-  - interacción con Game: cuando Game llame a los métodos step() y
-    draw() de un GameBoard que haya sido añadido como un board a Game,
-    GameBoard debe ocuparse de que se ejecuten los métodos step() y
-    draw() de todos los objetos que contenga
-
-  - debe ofrecer la posibilidad de detectar la colisión entre
-    objetos. Un objeto sprite almacenado en GameBoard debe poder
-    detectar si ha colisionado con otro objeto del mismo
-    GameBoard. Los misiles disparados por la nave del jugador deberán
-    poder detectar gracias a esta funcionalidad ofrecida por GameBoard
-    cuándo han colisionado con una nave enemiga; una nave enemiga debe
-    poder detectar si ha colisionado con la nave del jugador; un misil
-    disparado por la nave enemiga debe poder detectar si ha
-    colisionado con la nave del jugador. Para ello es necesario que se
-    pueda identificar de qué tipo es cada objeto sprite almacenado en
-    el tablero de juegos, pues cada objeto sólo quiere comprobar si ha
-    colisionado con objetos de cierto tipo, no con todos los objetos.
-
-*/
