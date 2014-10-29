296abfbe6ed5f1778adfc97f564a2353dd8c9a88
jalonsob <jalonsob1991@gmail.com>
2014-10-29 12:47:41 +0100

    Comprobados que funcionan las pruebas

--- 11-collision/spec/PlayerMissileSpec.js
+++ 11-collision/spec/PlayerMissileSpec.js
@@ -1,29 +1,45 @@
-/*
+describe("Clase PlayerMissileSpec", function(){
+	var canvas, ctx;
 
-  Requisitos: 
+	beforeEach(function(){
+		loadFixtures('index.html');
 
-  La nave del usuario disparará 2 misiles si está pulsada la tecla de
-  espacio y ha pasado el tiempo de recarga del arma.
+		canvas = $('#game')[0];
+		expect(canvas).toExist();
 
-  El arma tendrá un tiempo de recarga de 0,25s, no pudiéndose enviar
-  dos nuevos misiles antes de que pasen 0,25s desde que se enviaron
-  los anteriores
+		ctx = canvas.getContext('2d');
+		expect(ctx).toBeDefined();
+	
+   	});
 
+   	
+	it ("draw", function(){
 
+		spyOn(SpriteSheet, "draw");
+		SpriteSheet.map = {
+		    missile: { sx: 0, sy: 30, w: 2, h: 10, frames: 1 },
+		};
+		
+		var missile = new PlayerMissile(7, 7);
+		missile.draw(ctx);	    
 
-  Especificación:
+	 	expect(SpriteSheet.draw).toHaveBeenCalled();
+	      	expect(SpriteSheet.draw.calls[0].args[1]).toEqual("missile");
+		expect(SpriteSheet.draw.calls[0].args[2]).toEqual(missile.x);
+		expect(SpriteSheet.draw.calls[0].args[3]).toEqual(missile.y);		
+	});
+	
+	it ("step", function(){
+		var gameb = new GameBoard();
+		var missile = new PlayerMissile(7, 7);
+		
+		gameb.add(missile);
+		spyOn(gameb, "remove");
+	
+		gameb.step(1);
+		
+		expect(gameb.remove).toHaveBeenCalled();	
+	});
 
-  - Hay que añadir a la variable sprites la especificación del sprite
-    missile
+});
 
-  - Cada vez que el usuario presione la tecla de espacio se añadirán
-    misiles al tablero de juego en la posición en la que esté la nave
-    del usuario. En el código de la clase PlayerSip es donde tienen
-    que añadirse los misiles
-
-  - La clase PlayerMissile es la que implementa los misiles. Es
-    importante que la creación de los misiles sea poco costosa pues va
-    a haber muchos disparos, para lo cual se declararán los métodos de
-    la clase en el prototipo
-
-*/
